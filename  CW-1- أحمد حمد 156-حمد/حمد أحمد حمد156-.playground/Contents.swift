310d644c23ce31d5dc948736e357987c1b158aaa
jar1hamad <hamadjs@Hamads-MacBook-Pro.local>
2020-06-13 19:04:45 +0300

    Done

---
+++  CW-1- أحمد حمد 156-حمد/حمد أحمد حمد156-.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+let name = "hamad"
+let age = 18
+let height = 1.69
+let hobby1 = "coding"
+let hobby2 = "programming"
+print("\(str) Hi, my name is \(name)")
+print("\(str) my age is \(age)")
+print("\(str) i enjoy \(hobby1) and \(hobby2)")
+print("\(str) I'm \(height)m tall ")
