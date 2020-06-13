84477039bc5a5dc36334f018889af4a87244c191
jar1hamad <hamadjs@Hamads-MacBook-Pro.local>
2020-06-13 19:45:51 +0300

    Done

---
+++ حمد أحمد حمد156-.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var str = "Hello, playground"
+let myname = "hamad"
+let myage = 18
+let myheight = 1.69
+let myhobby1 = "coding"
+let myhobby2 = "programming"
+print("\(str) Hi, my name is \(myname)")
+print("\(str) my age is \(myage)")
+print("\(str) i enjoy \(myhobby1) and \(myhobby2)")
+print("\(str) I'm \(myheight)m tall ")
+///
+
+let friendname = "abdullah"
+var friendage = 18
+var friendheight = 1.71
+var friendhobby1 = "swimming"
+var friendhobby2 = "soccer"
+var ageDifference = myage - friendage
+var hieghtDifference = myheight - friendheight
+myhobby1 == friendhobby1
+myhobby1 > friendhobby1
+myhobby1 < friendhobby1
+myhobby1 >= friendhobby1
+myhobby1 <= friendhobby1
+myhobby2 == friendhobby2
+myhobby2 > friendhobby2
+myhobby2 < friendhobby2
+myhobby2 >= friendhobby2
+myhobby2 <= friendhobby2
+
+
+
